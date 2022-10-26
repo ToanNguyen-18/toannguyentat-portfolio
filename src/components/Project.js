22fccfe426441c700768b911de01484e16b15e2f
ToanNguyen-18 <toannguyentat18@gmail.com>
2022-10-26 17:23:08 +0700

    project starting

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,6 +1,39 @@
 import React, {useEffect, useState} from "react";
-
+import sanityClient from "../client.js";
 
 export default function Project() {
-    return 
+    const [projectDat, setProjectData] = useState(null);
+
+    useEffect(() => {
+        sanityClient.fetch(`*[_type == "project"]{
+            title,
+            date,
+            description,
+            projectType,
+            link,
+            tags
+        }`).then((data) => setProjectData(data))
+            .catch(console.error);
+    }, []);
+
+    return (
+        <main>
+            <section>
+                <h1></h1>
+                <h2></h2>
+                <section>
+                    <article>
+                        <h3></h3>
+                        <div>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <p></p>
+                            <span></span>
+                        </div>
+                    </article>
+                </section>
+            </section>
+        </main>
+    )
 }
