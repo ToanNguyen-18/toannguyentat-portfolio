c6a5b3bd1d5bfd76d1d5c97db4cb8e0236a458bf
ToanNguyen-18 <toannguyentat18@gmail.com>
2022-10-26 14:22:58 +0700

    NavBar

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import About from "./components/About";
 import SinglePost from "./components/SinglePost";
 import Post from "./components/Post";
 import Project from "./components/Project";
+import NavBar from "./components/NavBar";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/about" element={<About />} />
