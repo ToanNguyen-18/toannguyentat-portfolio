c44f54fb3d7aa9671705a16d9eab83357f817ac8
unknown <toannguyentat18@gmail.com>
2022-10-26 22:45:57 +0700

    singlePost againn

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -56,7 +56,7 @@ export default function SinglePost() {
                     style={{height: "400px"}}/>
                 </header>
                 <div className="px-16 lg:px-48 py-12 lg:py-20 prose lg:prose-xl max-w-full">
-                    BLOCK CONTENT
+                    <BlockContent blocks={singlePost.body} projectId="q99tg1aq" dataset="production"/>
                 </div>
             </article>
         </main>
