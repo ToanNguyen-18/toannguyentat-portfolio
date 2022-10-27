a3ae7369ac7830ee7533b70c91366cd584845721
ToanNguyen-18 <toannguyentat18@gmail.com>
2022-10-27 15:08:05 +0700

    add

--- src/index.js
+++ src/index.js
@@ -18,30 +18,30 @@
 
 //////////////////////////////////////////////////////////////////////
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter} from 'react-router-dom';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-);
-
-/////////////////////////////////////////////////////////
-
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
-// import { HashRouter } from 'react-router-dom';
+// import { BrowserRouter} from 'react-router-dom';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
-//     <HashRouter>
+//     <BrowserRouter>
 //         <App />
-//     </HashRouter>
+//     </BrowserRouter>
 // );
+
+/////////////////////////////////////////////////////////
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { HashRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <HashRouter>
+        <App />
+    </HashRouter>
+);
